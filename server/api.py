50da5da39b7417d1a37c0f72faede273f778fc1a
Asier Ochoa <asierxs05@gmail.com>
2023-01-19 00:32:30 -0400

    added create ad route, started list ads route

--- server/api.py
+++ server/api.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from tools import db_connect, DuplicatedEntityException
+from tools import db_connect, DuplicatedEntityException, EntityNotFoundException
 from configuration import config
 from providers import AdProvider
 
@@ -11,9 +11,13 @@ def handle_duplicated_entity(e):
     return jsonify({"response": f"Error: {e}"}), 422
 
 
+@api_routes.errorhandler(EntityNotFoundException)
+def handle_missing_entity(e):
+    return jsonify({"response": f"Error: {e}"}), 404
+
+
 @api_routes.get("/db_status")
 def get_db_status():
-    db_conf = config.mariadb
     db_conn = db_connect()
     return db_conn.server_info, 200
 
@@ -33,5 +37,37 @@ def post_ad_provider():
     conn = db_connect()
     ad_provider.submit(conn.cursor())
     conn.commit()
+
+    return jsonify({"response": "OK"}), 200
+
+
+@api_routes.get("/ad_provider/get")
+def get_ad_provider():
+    """
+    Request Body:
+    {
+        "company_name": <str>
+    }
+    """
+    body: dict = request.get_json(force=True)
+
+    conn = db_connect()
+    ret = AdProvider(**body, email="").get_provider(conn.cursor())
+    if ret is None:
+        raise EntityNotFoundException("Company with name {body['company_name']} could not be found!")
+
+    return ret, 200
+
+
+@api_routes.post("/ad_provider/<p_id>/create_ad")
+def post_ad(p_id):
+    """
+    Request Body:
+    """
+    body: dict = request.get_json(force=True)
+
+    conn = db_connect()
+    ad_provider = AdProvider.provider_factory_id(p_id, conn.cursor())  # Find provider using id
+    ad_provider.create_ad(**body, cursor=conn.cursor())
 
     return jsonify({"response": "OK"}), 200
--- server/providers.py
+++ server/providers.py
@@ -1,5 +1,6 @@
 from mariadb import Cursor
-from tools import rows_to_dict, DuplicatedEntityException
+from tools import rows_to_dict, DuplicatedEntityException, EntityNotFoundException
+import tools
 
 
 class SpaceProvider:
@@ -9,17 +10,33 @@ class SpaceProvider:
         self.iban: str = iban
 
     def submit(self, cursor: Cursor):
-        cursor.execute("""
-        INSERT INTO Space_provider(company_name, contact_email, payment_account_iban)
-        VALUES(?, ?, ?)
-        """,
-                       [self.company_name, self.email, self.iban])
+        cursor.execute(
+            """
+            INSERT INTO Space_provider(company_name, contact_email, payment_account_iban)
+            VALUES(?, ?, ?)
+            """,
+            [self.company_name, self.email, self.iban]
+        )
 
 
 class AdProvider:
-    def __init__(self, company_name, email):
+    def __init__(self, company_name, email, p_id):
         self.company_name: str = company_name
         self.email: str = email
+
+    @staticmethod
+    def provider_factory_id(p_id: int, cursor: Cursor):
+        cursor.execute(
+            """
+            SELECT id as p_id, company_name, contact_email as email FROM Ad_provider where id=?
+            """,
+            [p_id]
+        )
+        ad_provider = rows_to_dict(cursor, cursor.fetchone())
+        if ad_provider is None:
+            raise EntityNotFoundException(f"Could not find ad provider with id {p_id}")
+
+        return AdProvider(**ad_provider)
 
     def get_provider(self, cursor: Cursor):
         cursor.execute(
@@ -34,11 +51,44 @@ class AdProvider:
         existing_provider = self.get_provider(cursor)
         if existing_provider is not None:
             raise DuplicatedEntityException(f"Company {existing_provider['company_name']} already exists!")
-        cursor.execute("""
-        INSERT INTO Ad_provider(company_name, contact_email)
-        VALUES (?, ?)
-        """,
-                       [self.company_name, self.email])
+        cursor.execute(
+            """
+            INSERT INTO Ad_provider(company_name, contact_email)
+            VALUES (?, ?)
+            """,
+            [self.company_name, self.email]
+        )
 
-    def create_ad(self, cursor: Cursor):
-        pass
+    # Need to add function to set categories in aux table
+    def create_ad(self, name: str, provider_id: int, ad_type: str, content_route: str, redirect_url: str, cursor: Cursor):
+        # Get ad type id
+        ad_type_id = tools.get_content_type_id(ad_type, cursor)
+        if ad_type_id is None:
+            raise EntityNotFoundException(f"Could not find ad type with name {ad_type}")
+
+        # Check if provider has ad with same name
+        cursor.execute(
+            """
+            SELECT id FROM Ads where name=? and provider_id=?
+            """,
+            [name, provider_id]
+        )
+        existing_ad = rows_to_dict(cursor, cursor.fetchone())
+        if existing_ad is not None:
+            raise DuplicatedEntityException(f"Provider {self.company_name} already has ad called {name}")
+
+        # Insert new ad
+        cursor.execute(
+            """
+            INSERT INTO Ads(name, provider_id, type, content_route, redirect_url)
+            VALUES(?, ?, ?, ?, ?)
+            """,
+            [name, provider_id, ad_type_id, content_route, redirect_url]
+        )
+
+    def list_ads(self, cursor: Cursor):
+        cursor.execute(
+            """
+            SELECT * FROM Ads where provider_id=?
+            """
+        )
